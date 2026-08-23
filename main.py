e371127c5449abaf3fd643879cf3b045b12ebceb
sachin <skpkorba9009@gmail.com>
2026-08-23 22:01:31 +0530

    HTML Presonse

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 
 app = FastAPI()
 
+posts = [{"name" : "post 1" },{"name" : "post 2" },{"name" : "post 3" } ]
 
-@app.get("/")
+
+@app.get("/",response_class=HTMLResponse,include_in_schema=False)
 def home():
-    return {"message": "Welcome to Blog Fastapi"}
+    return "<h1>Welcome to Blog Fastapi</h1>"
+
+@app.get("/posts")
+def get_posts():
+    return posts
